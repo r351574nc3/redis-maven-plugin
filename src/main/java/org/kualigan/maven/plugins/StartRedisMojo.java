ff2a0f4383f26a64b99a40d03fe7bb8b7dbb43f7
Przybylski 중광 <r351574nc3@gmail.com>
2013-11-24 18:31:21 -0700

    Adding start mojo to start the redis server and release the server when the last connection finishes

--- src/main/java/org/kualigan/maven/plugins/StartRedisMojo.java
+++ src/main/java/org/kualigan/maven/plugins/StartRedisMojo.java
@@ -1,5 +1,3 @@
-package org.kualigan.maven.plugins;
-
 /*
  * Copyright 2001-2005 The Apache Software Foundation.
  *
@@ -15,6 +13,12 @@ package org.kualigan.maven.plugins;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.kualigan.maven.plugins;
+
+import redis.server.netty.RedisCommandDecoder;
+import redis.server.netty.RedisCommandHandler;
+import redis.server.netty.RedisReplyEncoder;
+import redis.server.netty.SimpleRedisServer;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -23,6 +27,16 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -40,7 +54,71 @@ public class StartRedisMojo extends AbstractMojo {
     @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
     private File outputDirectory;
 
+    /**
+     * Location of the file.
+     */
+    @Parameter( defaultValue = "${redis.port}", property = "port", required = false )
+    private int port;
+
     public void execute()
         throws MojoExecutionException {
+        try {
+            start();
+        }
+        catch (Exception e) {
+            throw new MojoExecutionException("Unable to start redis server", e);
+        }
+    }
+    
+    public void start() throws InterruptedException {
+        // Only execute the command handler in a single thread
+        final RedisCommandHandler commandHandler = new RedisCommandHandler(new SimpleRedisServer());
+
+        // Configure the server.
+        final ServerBootstrap b = new ServerBootstrap();
+        final DefaultEventExecutorGroup group = new DefaultEventExecutorGroup(1);
+        try {
+            b.group(new NioEventLoopGroup(), new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 100)
+                .localAddress(port)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            final ChannelPipeline p = ch.pipeline();
+//             p.addLast(new ByteLoggingHandler(LogLevel.INFO));
+                            p.addLast(new RedisCommandDecoder());
+                            p.addLast(new RedisReplyEncoder());
+                            p.addLast(group, commandHandler);
+                        }
+                    });
+
+            // Start the server.
+            final ChannelFuture f = b.bind().sync();
+
+            // Wait until the server socket is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            // Shut down all event loops to terminate all threads.
+            group.shutdownGracefully();
+        }
+    }
+
+    public void setOutputDirectory(final File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    public File getOutputDirectory() {
+        return this.outputDirectory;
+    }
+    
+    public void setPort(final int port) {
+        this.port = port;
+    }
+    
+    public int getPort() {
+        return port;
     }
 }
+
