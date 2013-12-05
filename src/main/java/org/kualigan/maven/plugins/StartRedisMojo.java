c367c79148c90380c569732e940196325e20e4b3
Przybylski 중광 <r351574nc3@gmail.com>
2013-12-05 07:57:18 -0700

    Adding forked and pre-integration test support

--- src/main/java/org/kualigan/maven/plugins/StartRedisMojo.java
+++ src/main/java/org/kualigan/maven/plugins/StartRedisMojo.java
@@ -43,72 +43,32 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Goal which touches a timestamp file.
+ * Start an in-memory redis server wherever want and whenever you want.
  *
+ * @author Leo Przybylski
  */
 @Mojo(name = "start", defaultPhase = LifecyclePhase.NONE, requiresProject = false)
-public class StartRedisMojo extends AbstractMojo {
-    public static final String REDIS_GROUP_CONTEXT_PROPERTY_NAME = StartRedisMojo.class.getName()
-        + File.pathSeparator + "redis";
-
+public class StartRedisMojo extends AbstractStartRedisMojo {
     /**
      * Redis server port number.
      */
-    @Parameter(property = "redis.server.port", defaultValue = "6379")
+    @Parameter(property = "redis.port", defaultValue = "6379")
     private int port;
+
+    /**
+     * Fork redis?
+     */
+    @Parameter(property = "redis.forked", defaultValue = "false")
+    private boolean forked;
+
 
     public void execute()
         throws MojoExecutionException {
         try {
-            start();
+            start(getForked());
         }
         catch (Exception e) {
             throw new MojoExecutionException("Unable to start redis server", e);
-        }
-    }
-
-    /**
-     * Start the redis server
-     *
-     */
-    public void start() throws InterruptedException {
-        // Only execute the command handler in a single thread
-        final RedisCommandHandler commandHandler = new RedisCommandHandler(new SimpleRedisServer());
-
-
-        // Configure the server.
-        final ServerBootstrap b = new ServerBootstrap();
-        final DefaultEventExecutorGroup group = new DefaultEventExecutorGroup(1);
-        getPluginContext().put(REDIS_GROUP_CONTEXT_PROPERTY_NAME, group);
-
-        try {
-            b.group(new NioEventLoopGroup(), new NioEventLoopGroup())
-                .channel(NioServerSocketChannel.class)
-                .option(ChannelOption.SO_BACKLOG, 100)
-                .localAddress(getPort())
-                .childOption(ChannelOption.TCP_NODELAY, true)
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            final ChannelPipeline p = ch.pipeline();
-                            p.addLast(new RedisCommandDecoder());
-                            p.addLast(new RedisReplyEncoder());
-                            p.addLast(group, commandHandler);
-                        }
-                    });
-
-            // Start the server.
-            getLog().info("Starting Redis(port=" + port + ") server...");
-            
-            final ChannelFuture f = b.bind();
-
-            // Add non-forked version later
-            // Wait until the server socket is closed.
-            // f.sync();
-            // f.channel().closeFuture().sync();
-        } finally {
-            // Shut down all event loops to terminate all threads.
-            // group.shutdownGracefully();
         }
     }
 
@@ -118,6 +78,14 @@ public class StartRedisMojo extends AbstractMojo {
     
     public int getPort() {
         return port;
+    }
+
+    public void setForked(final Boolean forked) {
+        this.forked = forked;
+    }
+    
+    public Boolean getForked() {
+        return forked;
     }
 }
 
